9381ef5d47f51fec47d68272e6a77f51dd11d04a
ENRIQUE TELLO BARBÉ <entelbar@upv.edu.es>
2020-12-09 17:26:39 +0100

    ff

--- train/preprocessing/get_formas.py
+++ train/preprocessing/get_formas.py
@@ -1,3 +1,103 @@
-    """
-    Buscar formas en las imagenes
-    """
+"""
+#OPCION 1
+# -*- coding: latin-1 -*-
+# import the necessary packages
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# show the original and blurred images
+cv2.imshow("Original", image)
+cv2.imshow("Blurred", blurred)
+
+# compute a "wide", "mid-range", and "tight" threshold for the edges
+wide = cv2.Canny(blurred, 10, 200)
+mid = cv2.Canny(blurred, 50, 150)
+tight = cv2.Canny(blurred, 240, 250)
+
+# show the edge maps
+cv2.imshow("Wide Edge Map", wide)
+cv2.imshow("Mid Edge Map", mid)
+cv2.imshow("Tight Edge Map", tight)
+cv2.waitKey(0)
+"""
+"""
+ * Python program to use a slider to control fixed-level
+ * thresholding value.
+ *
+ * usage: python interactive_thresholding.py <filename>
+"""
+"""
+OPCION 2
+import skimage
+import skimage.viewer
+import sys
+
+filename = sys.argv[1]
+
+
+def filter_function(image, sigma, threshold):
+    masked = image.copy()
+    masked[skimage.filters.gaussian(image, sigma=sigma) <= threshold] = 0
+    return masked
+
+
+smooth_threshold_plugin = skimage.viewer.plugins.Plugin(
+    image_filter=filter_function
+)
+
+smooth_threshold_plugin.name = "Smooth and Threshold Plugin"
+
+smooth_threshold_plugin += skimage.viewer.widgets.Slider(
+    "sigma", low=0.0, high=7.0, value=1.0
+)
+smooth_threshold_plugin += skimage.viewer.widgets.Slider(
+    "threshold", low=0.0, high=1.0, value=0.5
+)
+
+image = skimage.io.imread(fname=filename, as_gray=True)
+
+viewer = skimage.viewer.ImageViewer(image=image)
+viewer += smooth_threshold_plugin
+viewer.show()
+"""
+"""
+ * Python script to demonstrate Canny edge detection.
+ *
+ * usage: python CannyEdge.py <filename> <sigma> <low_threshold> <high_threshold>
+"""
+import skimage
+import skimage.feature
+import skimage.viewer
+import sys
+from skimage import viewer
+
+# read command-line arguments
+filename = sys.argv[1]
+sigma = float(sys.argv[2])
+low_threshold = float(sys.argv[3])
+high_threshold = float(sys.argv[4])
+
+# load and display original image as grayscale
+image = skimage.io.imread(fname=filename, as_gray=True)
+viewer = skimage.viewer(image=image)
+viewer.show()
+
+edges = skimage.feature.canny(
+    image=image,
+    sigma=sigma,
+    low_threshold=low_threshold,
+    high_threshold=high_threshold,
+)
+# display edges
+viewer = skimage.viewer.ImageViewer(edges)
+viewer.show()
